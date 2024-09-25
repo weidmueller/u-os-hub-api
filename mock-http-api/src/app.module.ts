7e38b8ac0eca3c79b2f2c501e629fd2846a377c0
Etienne Schmidt <etienne.schmidt@weidmueller.com>
2024-09-25 17:55:29 +0200

    chore: remove mock-http-api
    Signed-off-by: Etienne Schmidt <etienne.schmidt@weidmueller.com>

--- mock-http-api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HttpApiService } from './http-api/http-api.service';
-import { HttpApiController } from './http-api/http-api.controller';
-
-@Module({
-  controllers: [HttpApiController],
-  providers: [HttpApiService],
-})
-export class AppModule {}
