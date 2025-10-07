09226d85aa4c917714c359a95f8e832350f139ba
Etienne Schmidt <etienne.schmidt@weidmueller.com>
2025-10-07 12:42:46 +0000

    chore: update gen copyright headers script
    Signed-off-by: Etienne Schmidt <etienne.schmidt@weidmueller.com>

--- tools/gen-copyright-headers.sh
+++ tools/gen-copyright-headers.sh
@@ -8,12 +8,17 @@ script_path="$(readlink -f ${0})"
 script_dir="$(dirname ${script_path})"
 project_dir="$(dirname ${script_dir})"
 
-current_year=$(date +'%Y')
 copyright="Weidmueller Interface GmbH & Co. KG <oss@weidmueller.com>"
 
 set -x
 
-reuse annotate --license MIT --copyright "$copyright" --year $current_year --merge-copyrights --recursive --skip-unrecognised "$project_dir"
+# Only check modified files, ignore deleted files and files with less than 4 lines changed
+files=$(git diff --diff-filter=dcr -w --numstat origin/main..HEAD | awk '{if ($1+$2 > 3) print $3}')
 
-# # annotate flatbuffer files separately with explicit C style
-reuse annotate --license MIT --copyright "$copyright" --year $current_year --merge-copyrights --recursive --style c "$project_dir/flatbuffers"
+reuse annotate --license MIT --copyright "$copyright" --merge-copyrights --recursive --skip-unrecognised $files
+
+unrecognizedFiles=$(echo "$files" | grep '\.fbs$')
+
+if [ -n "$unrecognizedFiles" ]; then
+    reuse annotate --copyright="$copyright" --license=MIT --style=c --merge-copyrights $unrecognizedFiles
+fi
